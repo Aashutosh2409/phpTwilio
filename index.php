667a9d3c14bac194cdf59f230cd782af487cb394
aashutoshsingh <as03681n@pace.edu>
2018-10-01 12:21:04 -0400

    make it better

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "Hello";
  
 $sid = 'ACba3bf5ed690fa8365ed49f99ac29c0b4';
 $token = '0661f05d98e6fee38ab62e54e73baee2';
-$twilio = new Client($sid, $token);
+$twilio = new Services_Twilio($sid, $token);
 
 //$client = new Services_Twilio($sid, $token);
  

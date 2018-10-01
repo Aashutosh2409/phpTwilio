d033a1bc610cf079133279c37083d6309201fec7
aashutoshsingh <as03681n@pace.edu>
2018-10-01 14:40:37 -0400

    make the code best this time

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 // this line loads the library
-//
+
 require_once 'Twilio/autoload.php';
-require ('Twilio.php');
+//require ('Twilio.php');
 use Twilio\Rest\Client;
 
-echo "Hello";
+echo "Hello \n";
 
 $sid    = "ACba3bf5ed690fa8365ed49f99ac29c0b4";
 $token  = "0661f05d98e6fee38ab62e54e73baee2";
@@ -21,28 +21,3 @@ $message = $twilio->messages
     );
 
 print($message->sid);
-
-//$client = new Services_Twilio($sid, $token);
- 
-//$twilio->messages->create(array(
-//	'To' => "+12019854884",
-//	'From' => "+19292039232",
-//	'Body' => "Hello from AASHUTOSH SINGH CS 643 Fall 2018"
-//));
-
-//$twilio->accounts->messages->create(
-//	'+12019854884',
-//	array(
-//    'from' => '+19292039232',
-//    'body' => "Hello from AASHUTOSH SINGH CS 643 Fall 2018"
-//));
-
-//$message = $twilio->messages
-//    ->create("+12019854884", // to
-//        array(
-//            "from" => "+19292039232",
-//            "body" => "Hello from AASHUTOSH SINGH CS 643 Fall 2018"
-//        )
-//    );
-//
-//print($message->sid);

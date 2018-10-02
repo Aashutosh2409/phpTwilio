5e7862e2a434ead2889e1cf97d8d98108b4bc5f1
Aashutosh Singh <34499323+Aashutosh2409@users.noreply.github.com>
2018-10-01 20:47:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,16 @@ require_once 'Twilio/autoload.php';
 //require ('Twilio.php');
 use Twilio\Rest\Client;
 
-echo "Hello ";
-
-$sid    = "ACba3bf5ed690fa8365ed49f99ac29c0b4";
-$token  = "0661f05d98e6fee38ab62e54e73baee2";
+//Using Twilio Account authentication and token
+$sid    = "AXXXXXXXXXXXXXXXXXXXXXXXX";
+$token  = "0XXXXXXXXXXXXXXXXXXXXXXXX";
 $twilio = new Client($sid, $token);
 
+//Detail Information
+echo "<strong>----- SMS Details: -----</strong> </br> ";
+echo "<strong>Sender:</strong> +19292039232 </br>";
+echo "<strong>Recipient:</strong> +19173490168 </br>";
+echo "<strong>Message:</strong> Hello from AASHUTOSH SINGH CS 643 Fall 2018 </br>";
 
 $message = $twilio->messages
     ->create("+19173490168", // to
